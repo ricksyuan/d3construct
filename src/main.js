dcf058bb5d1945dbfa63c91e1a615315c4aadf89
Rick Yuan <rick.s.yuan@gmail.com>
2019-02-13 22:11:18 -0500

    Add zoom and pan

--- src/main.js
+++ src/main.js
@@ -30,17 +30,16 @@ const yScale = d3.scaleLinear()
   .domain(yDomain)
   .range([HEIGHT, 0]);
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  
-  d3.select('svg')
-    .style('width', WIDTH)
-    .style('height', HEIGHT);
-
-  drawAxis(runs);
-  render();
-  
-});
+function populateTable() {
+  const table = d3.select('tbody');
+  table.html('');
+  runs.forEach((run) => {
+    const row = table.append('tr');
+    row.append('td').html(run.id);
+    row.append('td').html(run.weight);
+    row.append('td').html(run.height);
+  });
+}
 
 function render() {
   d3.select('#points').html('');
@@ -51,53 +50,88 @@ function render() {
     .attr('cx', datum => xScale(datum.weight))
     .attr('cy', datum => yScale(datum.height));
 
+  populateTable();
+
+  function dragEnd(datum) {
+    const { x, y } = d3.event;
+    const weight = xScale.invert(x);
+    const height = yScale.invert(y);
+    datum.weight = weight;
+    datum.height = height;
+    populateTable();
+  }
+
+  // Drag cannot be fat arrow function.
+  function drag() {
+    const { x, y } = d3.event;
+    d3.select(this).attr('cx', x);
+    d3.select(this).attr('cy', y);
+  }
+  const dragBehavior = d3.drag()
+    .on('drag', drag)
+    .on('end', dragEnd);
+  d3.selectAll('circle').call(dragBehavior);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  d3.select('#container')
+    .style('width', WIDTH)
+    .style('height', HEIGHT);
+
+  const bottomAxis = d3.axisBottom(xScale);
+  d3.select('#container')
+    .append('g')
+    .attr('id', 'x-axis')
+    .call(bottomAxis)
+    .attr('transform', `translate(0, ${HEIGHT})`);
+  const leftAxis = d3.axisLeft(yScale);
+  d3.select('#container')
+    .append('g')
+    .attr('id', 'y-axis')
+    .call(leftAxis);
+
+  // Add zoom.
+  let lastTransform = null;
+  function zoomCallback() {
+    lastTransform = d3.event.transform;
+    d3.select('#points').attr('transform', d3.event.transform);
+    d3.select('#x-axis')
+      .call(bottomAxis.scale(d3.event.transform.rescaleX(xScale)));
+    d3.select('#y-axis')
+      .call(leftAxis.scale(d3.event.transform.rescaleY(yScale)));
+  }
+
+  const zoom = d3.zoom()
+    .on('zoom', zoomCallback);
+  d3.select('#container').call(zoom);
+
   // Add point when svg is clicked.
-  d3.select('svg').on('click', function () {
-    const x = d3.event.offsetX;
-    const y = d3.event.offsetY;
+  d3.select('#container').on('click', () => {
+    let x = d3.event.offsetX;
+    let y = d3.event.offsetY;
+    if (lastTransform !== null) {
+      x = lastTransform.invertX(d3.event.offsetX);
+      y = lastTransform.invertY(d3.event.offsetY);
+    }
     const weight = xScale.invert(x);
     const height = yScale.invert(y);
 
     const newRun = {
       id: (runs.length > 0) ? runs[runs.length - 1].id + 1 : 1,
-      weight: weight,
-      height: height
-    }
+      weight,
+      height,
+    };
     runs.push(newRun);
     render();
-    populateTable();
   });
 
   // Remove clicked point when clicked.
-  d3.selectAll('circle').on('click', (datum, index) => {
+  d3.selectAll('circle').on('click', (datum) => {
     // Prevent event from hitting svg.
     d3.event.stopPropagation();
     runs = runs.filter(run => run.id !== datum.id);
     render();
-    populateTable();
   });
-};
-
-function drawAxis() {
-
-  const bottomAxis = d3.axisBottom(xScale);
-  const leftAxis = d3.axisLeft(yScale);
-  d3.select('svg')
-    .append('g')
-    .call(leftAxis)
-    .append('g')
-    .call(bottomAxis)
-    .attr('transform', `translate(0, ${HEIGHT})`);
-
-}
-
-function populateTable() {
-  const table = d3.select('tbody')
-  table.html('');
-  runs.forEach(run => {
-    const row = table.append('tr');
-    row.append('td').html(run.id);
-    row.append('td').html(run.weight);
-    row.append('td').html(run.height);
-  });
-}
+  // Draw points and table.
+  render();
+});
